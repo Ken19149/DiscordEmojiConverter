4d09045b3649c0cc20e80ff34534753d51317511
Ken980 <kenboy9800@gmail.com>
2024-06-06 23:55:44 +0700

    optimize replace number

--- disEmoConverter.py
+++ disEmoConverter.py
@@ -4,9 +4,11 @@ while True:
     text = input()
     for i in text:
         if i.lower() in "abcdefghijklmnopqrstuvwxyz":
-            output = output + (" :regional_indicator_" + i + ": ")
-        elif i in "0123456789!?":
-            output = output + (i.replace("1"," :one: ").replace("2"," :two: ").replace("3"," :three: ").replace("4"," :four: ").replace("5"," :five: ").replace("6"," :six: ").replace("7"," :seven: ").replace("8"," :eight: ").replace("9"," :nine: ").replace("0"," :zero: ").replace("?", " :question: ").replace("!", " :exclamation: "))
+            output = output + (":regional_indicator_" + i + ": ")
+        elif i in "0123456789":
+            output = output + (":number_" + i + ": ")
+        elif i in "?!":
+            output = output + (i.replace("?", " :question: ").replace("!", " :exclamation: "))
         elif i == " ":
             output = output + (i.replace(" ", "  "))
         else:
